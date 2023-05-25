54edc86733fec859ff4f919c7c889570c2eb9c6c
Sebastian Duenas <jsduenass@unal.edu.co>
2023-05-25 10:12:14 -0500

    [FIX]: inverse kinematics math formulas - fix PincherPose theta data type - add launch file
    Signed-off-by: Sebastian Duenas <jsduenass@unal.edu.co>

--- scripts/pincher.py
+++ scripts/pincher.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import rospy
 import numpy as np
 
@@ -8,6 +10,7 @@ import spatialmath as sm
 from numpy import pi, typing
 
 from spatialmath import SE3
+from spatialmath.base import tr2rpy
 
 # messages
 from std_msgs.msg import String
@@ -16,7 +19,8 @@ from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from robotics_lab5.msg import PincherPose
 
-class pincher():
+
+class Pincher():
     def __init__(self):
         # Robot parameters [mm]
         self.L1=152.05
@@ -36,51 +40,71 @@ class pincher():
     
         self.jStateSub = rospy.Subscriber("/dynamixel_workbench/joint_states", JointState,self.fkinematics)
             
-        self.jTrajecPub = rospy.Publisher('/joint_trajectory', JointTrajectory, queue_size=0)
+        self.jTrajecPub = rospy.Publisher('/joint_trajectory', JointTrajectory, queue_size=10)
         
-    def rosCommunication(self):
+        self.positionPub = rospy.Publisher("/real_pincher_pose", PincherPose,queue_size=10)
+        
+    #def rosCommunication(self):
 
         while not rospy.is_shutdown():
             #print(state)
             rospy.loginfo("pincher running") 
         
-            rospy.sleep(1)
+            rospy.sleep(30)
 
     def fkinematics(self,JointState_msg):
-        q = JointState_msg.position
-        
+        #rostopic pub -r 10 /dynamixel_workbench/joint_states trajectory_msgs/JointTrajectory  '{linear:  {x: 0.1, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
+        q_extended = JointState_msg.position
+        q = q_extended[0:-1]  
         MTH = self.robot.fkine(q)
-        position  =MTH.t
-
-        pass
-
+        
+        real_pose = PincherPose()
+        
+        pos_vector  = MTH.t
+        point = Point()
+        point.x = pos_vector[0]
+        point.y = pos_vector[1]
+        point.z = pos_vector[2]
+        real_pose.point = point
+        
+        rpy_angles= MTH.rpy()
+        real_pose.theta = rpy_angles[1]
+        rospy.loginfo(real_pose)
+        rospy.loginfo(real_pose.point)
+        #vector_pos = [real_pose.point.x, real_pose.point.y, real_pose.point.z, real_pose.theta]
+        
+        self.positionPub.publish(real_pose)
+        #rospy.loginfo("pincher fkinematics pose"+ str(np.round(vector_pos,3))) 
+    
     def ikinematics(self,PincherPose_msg):
+        #rostopic pub -r 10 /target_pincher_pose robotics_lab5/PincherPose  '{point:  {x: 100, y: 200.0, z: 20.0}, theta: -0.5}'
         L1 = self.L1
         L2 = self.L2
         L3 = self.L3
         L4 = self.L4
         
+        Point_msg = PincherPose_msg.point 
         x = Point_msg.x
         y = Point_msg.y
         z = Point_msg.z
         
         theta = PincherPose_msg.theta
-        Point_msg = PincherPose_msg.Point 
         
         q1 = np.arctan2(y,x)
         
         q4 = theta
-        
-        wrist_center = SE3.Trans(-L4, 0, 0)*SE3.Ry(-q4)*SE3.Rz(q1)*SE3.Trans(x, y, z)
+        MTH6 = SE3.Trans(x, y, z)*SE3.Rz(q1)*SE3.Ry(q4)
+
+        wrist_center = MTH6*SE3.Trans(-L4, 0, 0)
         pos_wrist_center =wrist_center.t
-        
+
         x_c = pos_wrist_center[0]
         y_c = pos_wrist_center[1]
         z_c = pos_wrist_center[2]
-        
+
         L = np.sqrt((z_c-L1)**2 + x_c**2 +y_c**2)
 
-        c_q3 = (L-(L2**2 + L3**2) )/(2*L2*L3)
+        c_q3 = (L**2-(L2**2 + L3**2) )/(2*L2*L3)
         s_q3 = np.sqrt(1 - c_q3**2)
         q3 = np.arctan2(s_q3,c_q3)
         
@@ -90,9 +114,26 @@ class pincher():
         q2 = gamma - alpha
 
         q = [q1,q2,q3,q4]
+        gripperIsOpen = True
         
-        print(q)
-
+        if gripperIsOpen:
+            q5 = 0
+        else :
+            q5 = 100    
+        q.append(q5)
+        
+        
+        state = JointTrajectory()        
+        state.header.stamp = rospy.Time.now()
+        state.joint_names = ["joint_1","joint_2","joint_3","joint_4","joint_5"]
+        point = JointTrajectoryPoint()
+        point.positions = q
+        point.time_from_start = rospy.Duration(0.5)
+        state.points.append(point)
+        self.jTrajecPub.publish(state)
+        
+        rospy.loginfo("pincher ikinematics "+ str(np.round(q,3)) )
+        
         pass
 
 
@@ -102,8 +143,8 @@ if __name__ == '__main__':
     rospy.init_node('pincher_node',anonymous=False)
     try:
         
-        robot = pincher()
-        robot.rosCommunication()
+        robot = Pincher()
+        #robot.rosCommunication()
 
     except rospy.ROSInterruptException:
         pass
